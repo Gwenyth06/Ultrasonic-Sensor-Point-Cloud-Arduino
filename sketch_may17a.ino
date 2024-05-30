f5cb376f461c4a0da77f48298c8a25c05e7c205f
Kardelen Duran <wildsnail0603@gmail.com>
2024-05-30 22:58:34 +0300

    Trigonometric calculations added
    And a little illustration for visualizing.

--- sketch_may17a.ino
+++ sketch_may17a.ino
@@ -1,12 +1,15 @@
 #include <LCD_I2C.h>
 #include <Servo.h>
+#include <math.h>
 
 Servo sg90, sg902;
 
 long duration;
 int distance;
 
-int pos = 0;
+int deg = 0;
+int deg2 = 0;
+float adjacent,opposite,rad = 0;
 
 const int trigPin = 10;
 const int echoPin = 11;
@@ -19,37 +22,46 @@ void setup() {
 
   sg90.attach(2);
   sg902.attach(3);
+
+  sg90.write(deg);
+  sg902.write(deg2);
   //lcd.print("Back2TheFuture");
   Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
+  if(deg <= 180) {
+    for (deg2 = 0; deg2 <= 180; deg2++) {
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(2);
 
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);
 
-  duration = pulseIn(echoPin, HIGH);
+      duration = pulseIn(echoPin, HIGH);
 
-  distance = duration * 0.034 / 2;
+      distance = duration * 0.034 / 2;
 
-  for ( pos = 0; pos <= 180; pos++) {
-    sg90.write(pos);
-    sg902.write(pos);
-    delay(15);
-  }
-  for ( pos = 180; pos >= 0; pos--) {
-    sg90.write(pos);
-    sg902.write(pos);
-    delay(15);
+      rad = radians(deg);
+      opposite = distance * sin(rad);
+      adjacent = distance * cos(rad);
+
+      sg902.write(deg2);
+      delay(1000);
+    }
+    pos = pos + 5;
+    sg90.write(deg);
+  } else {
+
   }
 
   Serial.print("Distance: ");
   Serial.println(distance);
-
-  //delay(3000);
-
+  Serial.print("pos: ");
+  Serial.println(pos);
+  Serial.print("pos2: ");
+  Serial.println(pos2);
+  delay(1000);
 }
